2c4177dacfab18152c48891ae7763c526b51fe43
zalegzl <zalegzl@gmail.com>
2018-12-27 15:36:46 +0800

    添加Java8日期时间转换

--- src/main/java/com/zale/sns/SnsApplication.java
+++ src/main/java/com/zale/sns/SnsApplication.java
@@ -2,13 +2,23 @@ package com.zale.sns;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
 
 @SpringBootApplication
+@EntityScan(basePackageClasses = { SnsApplication.class, Jsr310JpaConverters.class })
 public class SnsApplication {
+
+	@PostConstruct
+	void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SnsApplication.class, args);
 	}
 
 }
-
